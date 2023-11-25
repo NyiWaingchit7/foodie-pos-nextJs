2dbbfd3885037ae35fe66fd5f292e6e3695a7cce
Your Name <you@example.com>
2023-11-25 12:15:22 +0630

    foodie-pos

--- src/pages/component/layout/BackOfficeLayout.tsx
+++ src/pages/component/layout/BackOfficeLayout.tsx
@@ -1,34 +1,39 @@
-import { Box } from "@mui/material";
-import { ReactNode, useEffect } from "react";
-import Topbar from "./Topbar";
-import SideBar from "./Sidebar";
-import { useSession } from "next-auth/react";
-
-import { fetchAppData } from "@/store/slice/appSlice";
 import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { fetchAppData } from "@/store/slices/appSlice";
+import { Box } from "@mui/material";
+import { useSession } from "next-auth/react";
+import { ReactNode, useEffect } from "react";
+import SideBar from "./Sidebar";
+import Topbar from "./Topbar";
 
-interface prop {
+interface Props {
   children: ReactNode;
 }
 
-const BackOfficeLayout = ({ children }: prop) => {
-  const { data } = useSession();
+const BackofficeLayout = ({ children }: Props) => {
+  const { data: session } = useSession();
   const dispatch = useAppDispatch();
   const { init } = useAppSelector((state) => state.app);
+
   useEffect(() => {
-    if (data && !init) {
+    if (session && !init) {
       dispatch(fetchAppData({}));
     }
-  }, [data]);
+  }, [session]);
 
   return (
     <Box>
       <Topbar />
-      <Box sx={{ display: "flex", position: "relative", zIndex: 5, flex: 1, }}>
-        {data && <SideBar />}
+      <Box sx={{ display: "flex", position: "relative", zIndex: 5, flex: 1 }}>
+        {session && (
+          <Box sx={{ display: { xs: "none", sm: "block" } }}>
+            <SideBar />
+          </Box>
+        )}
         <Box sx={{ p: 3, width: "100%", height: "100%" }}>{children}</Box>
       </Box>
     </Box>
   );
 };
-export default BackOfficeLayout;
+
+export default BackofficeLayout;
